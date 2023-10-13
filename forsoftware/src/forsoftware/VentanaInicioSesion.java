0f2ac75242cec779bb62635522aea64db143962f
Ivan <ivan.renteria@digipen.edu>
2023-10-13 20:34:23 +0200

    Continuacion ventana inicio de sesion

--- forsoftware/src/forsoftware/VentanaInicioSesion.java
+++ forsoftware/src/forsoftware/VentanaInicioSesion.java
@@ -1,39 +1,45 @@
 package forsoftware;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class VentanaInicioSesion extends JFrame {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel pNorte, pSur, pCentro;
 	private JTextField correo;
 	private JButton confirmar; 
+	private JLabel titulo;
 	
 	public VentanaInicioSesion() {
 		super();
 		setBounds(300, 300, 600, 400);
 		pNorte = new JPanel();
 		pSur = new JPanel();
-		pCentro = new JPanel(new GridLayout(2, 1));
+		pCentro = new JPanel(new GridBagLayout());
 		getContentPane().add(pNorte,BorderLayout.NORTH);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
 		getContentPane().add(pCentro,BorderLayout.CENTER);
 		
+	
+		titulo = new JLabel("Agrega correo de inicio de sesión");
+		titulo.setHorizontalAlignment(JLabel.CENTER);
+		pNorte.add(titulo);
 		correo = new JTextField(20);
-		correo.setBounds(50, 50, 200, 25);
 		confirmar = new JButton("Confirmar");
-		confirmar.setBounds(100, 100, 100, 25);
 		
-		pCentro.add(correo);
-		pCentro.add(confirmar);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+
+		pCentro.add(correo, gbc);
+		pCentro.add(confirmar, gbc);
 		
 		confirmar.addActionListener(e -> {
             String text = correo.getText();
@@ -44,14 +50,12 @@ public class VentanaInicioSesion extends JFrame {
             }
         });
 		
-		
 		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
 		new VentanaInicioSesion();
 	}
-
-
-	
 }
+
+
