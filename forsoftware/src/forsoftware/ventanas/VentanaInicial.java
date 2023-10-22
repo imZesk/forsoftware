b7365f5ef4923aff8f59b0b572e51b71be38d5f3
Marco Fernandez <marco.fernandez@opendeusto.es>
2023-10-22 16:10:41 +0200

    Rediseñada Pestaña 1 [añadido una foto, estilizado la lista de tareas, añadido un texto no editable], reescrito los bloques de codigo de cada pestaña.

--- forsoftware/src/forsoftware/ventanas/VentanaInicial.java
+++ forsoftware/src/forsoftware/ventanas/VentanaInicial.java
@@ -1,67 +1,85 @@
 package forsoftware.ventanas;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextArea;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
 
 public class VentanaInicial extends JFrame {
-
     public VentanaInicial() {
-        // Configura la ventana
-        setTitle("Bienvenido %s - Info resumida nombre etc etc");
-        setSize(1200, 700);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Crea el panel que contendrá las pestañas
-        JTabbedPane tabbedPane = new JTabbedPane();
+        JTabbedPane tabs = new JTabbedPane();
 
-        // Crea los paneles para cada pestaña
+        // Pestaña 1
         JPanel panel1 = new JPanel(new BorderLayout());
+
+        // Añadir imagen 
+        ImageIcon imagen = new ImageIcon("C:/Users/ytzes/Downloads/usuario.jpg");
+        JLabel etiquetaImagen = new JLabel(imagen);
+        panel1.add(etiquetaImagen, BorderLayout.WEST);
+
+        // Añadir panel de texto no editable debajo de la imagen
+        JTextArea texto = new JTextArea("Datos del empleado [por añadir]");
+        texto.setEditable(false);
+        panel1.add(etiquetaImagen, BorderLayout.NORTH);
+        panel1.add(texto, BorderLayout.CENTER);
+
+        // Añadir 3 botones debajo de la ventana
+        JPanel panelBotones = new JPanel(new GridLayout(1, 3));
+        JButton boton1 = new JButton("Boton 1");
+        JButton boton2 = new JButton("Boton 2");
+        JButton boton3 = new JButton("Boton 3");
+        panelBotones.add(boton1);
+        panelBotones.add(boton2);
+        panelBotones.add(boton3);
+        panel1.add(panelBotones, BorderLayout.SOUTH);
+
+        // Añadir lista con Jscroll a la derecha
+        String[] elementosLista = {"Tarea 1", "Tarea 2", "Tarea 3", "Tarea 4", "Tarea 5", "Tarea 6", "Tarea 7", "Tarea 8", "Tarea 9", "Tarea 10"};
+        JList<String> lista = new JList<>(elementosLista);
+        JScrollPane scrollLista = new JScrollPane(lista);
+        panel1.add(scrollLista, BorderLayout.LINE_END);
+        scrollLista.setPreferredSize(new Dimension(150, 0)); // Ancho del scroll
+        lista.setFixedCellHeight(30); // aalto de los elementos de la lista
         
-        
-        JTextArea textoPanel1 = new JTextArea("Resumen perfil trabajador");
-        panel1.add(textoPanel1, BorderLayout.WEST);
-        String[] listaTareas = {"Tarea 1", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3", "Tarea 2", "Tarea 3"};
-        JList<String> lista = new JList<>(listaTareas);
-        panel1.add(new JScrollPane(lista), BorderLayout.EAST);
-        
+
+        // Añadir pestaña 1 a las tabs
+        tabs.addTab("Pestaña 1", panel1);
+// ========================================= FIN PESTAÑA 1 =========================================
+        // Pestaña 2
         JPanel panel2 = new JPanel();
+        JLabel etiqueta2 = new JLabel("Contenido de la pestaña 2");
+        panel2.add(etiqueta2);
+        tabs.addTab("Pestaña 2", panel2);
+
+// ========================================= FIN PESTAÑA 2 =========================================        
+        
+        // Pestaña 3
         JPanel panel3 = new JPanel();
+        JLabel etiqueta3 = new JLabel("Contenido de la pestaña 3");
+        panel3.add(etiqueta3);
+        tabs.addTab("Pestaña 3", panel3);
+
+// ========================================= FIN PESTAÑA 3 =========================================
+
+        // Pestaña 4
         JPanel panel4 = new JPanel();
+        JLabel etiqueta4 = new JLabel("Contenido de la pestaña 4");
+        panel4.add(etiqueta4);
+        tabs.addTab("Pestaña 4", panel4);
 
-        // Agrega contenido a cada panel
-        panel2.add(new JLabel("Contenido de Pestaña 2"));
-        panel3.add(new JLabel("Contenido de Pestaña 3"));
-        panel4.add(new JLabel("Contenido de Pestaña 4"));
+// ========================================= FIN PESTAÑA 4 =========================================
+        
+        // Añadir tabs a la ventana
+        add(tabs);
 
-        // Agrega las pestañas al JTabbedPane
-        tabbedPane.addTab("Pestaña 1", panel1);
-        tabbedPane.addTab("Pestaña 2", panel2);
-        tabbedPane.addTab("Pestaña 3", panel3);
-        tabbedPane.addTab("Pestaña 4", panel4);
-
-        // Agrega el JTabbedPane a la ventana
-        getContentPane().add(tabbedPane, BorderLayout.CENTER);
-
-        // Hace visible la ventana
+        // Configurar ventana
+        setTitle("Bienvenido, [] - ForSoftware");
+        setSize(1200, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Ejecuta la aplicación
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new VentanaInicial();
-            }
-        });
+        new VentanaInicial();
     }
 }
-
-
