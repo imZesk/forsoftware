7f93fe5693a18f182216ecff78df98811be21b40
Marco Fernandez <marco.fernandez@opendeusto.es>
2023-10-26 21:52:53 +0200

    Ahora la VentanaTrabajadores puede leer los trabajadores y cargarlos mediante un CSV

--- forsoftware/src/forsoftware/ventanas/VentanaTrabajadores.java
+++ forsoftware/src/forsoftware/ventanas/VentanaTrabajadores.java
@@ -1,5 +1,8 @@
 package forsoftware.ventanas;
 import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -14,42 +17,51 @@ public class VentanaTrabajadores extends JFrame{
 
         // Crear los datos de ejemplo 
         String[] columnas = {"ID", "Nombre", "Apellido", "Género", "Provincia", "Telefono", "Correo de empresa", "Sueldo"};
-        String[][] datos = {
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-            {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-        };
+        String[][] datos = {};
 
-       // DefaultTableModel model = new DefaultTableModel(datos, columnas);
+        
+        // DefaultTableModel model = new DefaultTableModel(datos, columnas);
 
-		DefaultTableModel model = new DefaultTableModel(datos, columnas) {
-			private static final long serialVersionUID = 1L;
+ 		DefaultTableModel model = new DefaultTableModel(datos, columnas) {
+ 			private static final long serialVersionUID = 1L;
 
-			@Override
-            public boolean isCellEditable(int row, int column) {
-                return false; // Hace que todas las celdas sean de solo lectura
-            }
-        };
+ 			@Override
+             public boolean isCellEditable(int row, int column) {
+                 return false; // Hace que todas las celdas sean de solo lectura
+             }
+         };
+        
+		String fichero = "src/Trabajadores.csv";
+		String line = "";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
+
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(";");
+				model.addRow(data);
+				//System.out.println("ID: " + data[0] + " Nombre: " + data[1] + " Apellido: " + data[2] + " Género: "
+				//		+ data[3] + " Provincia: " + data[4] + " Teléfono: " + data[5] + " Correo de empresa: "
+				//		+ data[6] + " Sueldo: " + data[8]);
+				System.out.println(data);
+				
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
         JTable tabla = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(tabla);
-        JPanel panelBotones = new JPanel();
+        JPanel panelBotones2 = new JPanel();
+        
+        ventana.add(scrollPane, BorderLayout.CENTER);
+        ventana.add(panelBotones2, BorderLayout.SOUTH);
         
         JButton botonAnyadir = new JButton("Añadir trabajador");
         JButton botonEliminar = new JButton("Eliminar trabajador");
-        panelBotones.add(botonAnyadir);
-        panelBotones.add(botonEliminar);
-        
-        ventana.add(scrollPane, BorderLayout.CENTER);
-        ventana.add(panelBotones, BorderLayout.SOUTH);
-
+        panelBotones2.add(botonAnyadir);
+        panelBotones2.add(botonEliminar);
        
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setSize(700, 350);
