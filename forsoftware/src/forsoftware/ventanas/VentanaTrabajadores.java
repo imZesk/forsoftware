6537fe6453812d7991aaa60f44b8707a16fcbb62
Ivan <ivan.renteria@digipen.edu>
2023-10-28 19:30:53 +0200

    modificacion del boton añadir trabajadores

--- forsoftware/src/forsoftware/ventanas/VentanaTrabajadores.java
+++ forsoftware/src/forsoftware/ventanas/VentanaTrabajadores.java
@@ -14,19 +14,17 @@ public class VentanaTrabajadores extends JPanel{
 	public VentanaTrabajadores() {
 
         // Crear los datos de ejemplo 
-        String[] columnas = {"ID", "Nombre", "Apellido", "Género", "Provincia", "Telefono", "Correo de empresa", "Sueldo"};
+        String[] columnas = {"ID", "Nombre", "Apellido", "Género", "Puesto", "Provincia", "Telefono", "Correo de empresa", "Sueldo"};
         String[][] datos = {
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
-                {"1112", "Juan", "Pérez", "hombre", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
+                {"1112", "Juan", "Pérez", "hombre", "programador", "Vizcaya","111111111", "Juan.Perez@forsoftware.es", "5000.00"},
             };
 
         
@@ -96,15 +94,56 @@ public class VentanaTrabajadores extends JPanel{
         });
         
         botonAnyadir.addActionListener(e ->{
-                String idTrabajador = JOptionPane.showInputDialog("Introduce el ID del trabajador");
-                if(idTrabajador != null && !idTrabajador.isEmpty()){
-                    model.addRow(new Object[]{idTrabajador, "Nombre", "Apellido", "Género", "Provincia", "Telefono", "Correo de empresa", "Sueldo"});
-                }else{
-                    JOptionPane.showMessageDialog(null, "Por favor, introduce un ID válido");
-                }
-            
-        });
+            String idTrabajador = JOptionPane.showInputDialog("Introduce el ID del trabajador");
+            while(idTrabajador != null && !idTrabajador.matches("\\d+")) {
+                JOptionPane.showMessageDialog(null, "Por favor, introduce un ID válido (solo números)");
+                idTrabajador = JOptionPane.showInputDialog("Introduce el ID del trabajador");
+            }
 
+            String nombre = JOptionPane.showInputDialog("Introduce el nombre del trabajador");
+            while(nombre != null && !nombre.matches("[a-zA-Z]+")) {
+                JOptionPane.showMessageDialog(null, "Por favor, introduce un nombre válido (solo letras)");
+                nombre = JOptionPane.showInputDialog("Introduce el nombre del trabajador");
+            }
+
+            String apellido = JOptionPane.showInputDialog("Introduce el apellido del trabajador");
+            while(apellido != null && !apellido.matches("[a-zA-Z]+")) {
+                JOptionPane.showMessageDialog(null, "Por favor, introduce un apellido válido (solo letras)");
+                apellido = JOptionPane.showInputDialog("Introduce el apellido del trabajador");
+            }
+
+            Object[] generos = {"Hombre", "Mujer", "Otros"};
+            String sexo = (String)JOptionPane.showInputDialog(null, "Selecciona el género", "Género", JOptionPane.QUESTION_MESSAGE, null, generos, generos[0]);
+
+            Object[] puestos = {"Programador", "Modelador", "GameDesigner", "Artista", "Escritor", "Video", "Sonido"};
+            String puesto = (String)JOptionPane.showInputDialog(null, "Selecciona el puesto", "Puesto", JOptionPane.QUESTION_MESSAGE, null, puestos, puestos[0]);
+
+            String provincia = JOptionPane.showInputDialog("Introduce la provincia del trabajador");
+            while(provincia != null && !provincia.matches("[a-zA-Z]+")) {
+                JOptionPane.showMessageDialog(null, "Por favor, introduce una provincia válida (solo letras)");
+                provincia = JOptionPane.showInputDialog("Introduce la provincia del trabajador");
+            }
+            
+            String telefono = JOptionPane.showInputDialog("Introduce el teléfono del trabajador");
+            while(telefono != null && !telefono.matches("\\d{9}")) {
+                JOptionPane.showMessageDialog(null, "Por favor, introduce un teléfono válido (exactamente 9 dígitos)");
+                telefono = JOptionPane.showInputDialog("Introduce el teléfono del trabajador");
+            }
+
+            String sueldo = JOptionPane.showInputDialog("Introduce el sueldo del trabajador");
+            while(sueldo != null && !sueldo.matches("\\d+")) {
+                JOptionPane.showMessageDialog(null, "Por favor, introduce un sueldo válido (solo números)");
+                sueldo = JOptionPane.showInputDialog("Introduce el sueldo del trabajador");
+            }
+
+            String correoEmpresa = nombre + "." + apellido + "@forsoftware.es";
+
+            if(idTrabajador != null && nombre != null && apellido != null && sexo != null && puesto != null && provincia != null && telefono != null && sueldo != null){
+                model.addRow(new Object[]{idTrabajador, nombre, apellido, sexo, puesto, provincia, telefono, correoEmpresa, sueldo});
+            }else{
+                JOptionPane.showMessageDialog(null, "Por favor, introduce datos válidos");
+            }
+        });
 
 
     }
