64ffbccee6050f5641548016a7b7874a9cf5cad2
Uthereted <131401333+Uthereted@users.noreply.github.com>
2023-10-29 00:02:03 +0200

    boton añadir definitivo sin excel

--- forsoftware/src/forsoftware/ventanas/VentanaProyectos.java
+++ forsoftware/src/forsoftware/ventanas/VentanaProyectos.java
@@ -66,25 +66,58 @@ public class VentanaProyectos extends JPanel{
 
 		});
 		botonAnyadir.addActionListener(e ->{
-			String[] nomDatos = {"ID", "nombre", "numeroDeParticipante", "fechaInicio", "fehcaAcabadoEstimado", "gastos", "TipoDeProyecto", "EstadoProyecto"};
+			String[] nomDatos = {"ID (solo 4 digitos)", "nombre (solo letras) ", "numeroDeParticipante (solo numeros)", "fechaInicio (con formato dd/mm/yyyy)", "fehcaAcabadoEstimado (con formato dd/mm/yyyy)", 
+					"gastos (numeros con dos decimales)", "TipoDeProyecto (Software o Proyectos)", "EstadoProyecto"};
 			String[] valores = new String[nomDatos.length];
-			for (int i = 0; i < nomDatos.length; i++) {
-				while (valores[i] == null || valores[i].isEmpty()) {
+
+			//el estado del proyecto al crearlo siempre es pendiente
+			valores[nomDatos.length - 1] = "Pendiente";
+
+			for (int i = 0; i < nomDatos.length-1; i++) {  //length-1 par q no pregunte el estado q sera de prederteminado como "pendiente"
+				boolean validar = false;
+				while (!validar) {
 					valores[i] = JOptionPane.showInputDialog("Ingrese " + nomDatos[i] + ": ");
+					
+					// cancelar el proceso de añadir si se pulsa "Cancelar" o cierra la ventana
 					if (valores[i] == null) {
-						JOptionPane.showMessageDialog(null, "Debe proporcionar un valor para " + nomDatos[i]);
+	                    return; // Salir del programa
+	                }
+					
+					
+					
+					if (!valores[i].isEmpty()) {
+						validar = true;
+
+						// Aplicar limitaciones específicas para cada campo
+						if (i == 0 && !valores[i].matches("\\d{4}")) { // ID (4 dígitos)
+							validar = false;
+						} else if (i == 1 && !valores[i].matches("[a-zA-Z]+")) { // Nombre (solo letras)
+							validar = false;
+						} else if (i == 2 && !valores[i].matches("\\d+")) { // numeroDeParticipante (solo numeros)
+							validar = false;
+						} else if (i == 3 && !valores[i].matches("\\d{2}/\\d{2}/\\d{4}")) { // fechaInicio (con formato dd/mm/yyyy)
+							validar = false;
+						} else if (i == 4 && !valores[i].matches("\\d{2}/\\d{2}/\\d{4}")) { // fehcaAcabadoEstimado (con formato dd/mm/yyyy)
+							validar = false;
+						} else if (i == 5 && !valores[i].matches("\\d+\\.\\d{2}")) { // gastos (numeros con dos decimales)
+							validar = false;
+						} else if (i == 6 && !valores[i].equalsIgnoreCase("Software") && !valores[i].equalsIgnoreCase("Multimedia")) { // TipoDeProyecto (Software o Proyectos)
+							validar = false;
+						}
 					}
+					if (!validar) {
+	                    // Si se hace clic en Cancelar, se proporciona una entrada vacía o la entrada no cumple con la validación, se solicitará nuevamente.
+	                    JOptionPane.showMessageDialog(null, "Debe proporcionar un valor válido para " + nomDatos[i]);
 				}
 			}
-			
-			for (int i = 0; i < nomDatos.length; i++) {
-				System.out.println(nomDatos[i] + ": " + valores[i]);
-			}
+		}
+			model.addRow(valores);
+		
 
 
 
 
-			/*String datoProyecto = JOptionPane.showInputDialog("Introduce el " + nomDatos[i] + " del proyecto");
+			/*
 	            if(datoProyecto != null && !datoProyecto.isEmpty()){
 	                model.addRow(new Object[]{idTrabajador, "Nombre", "Apellido", "Género", "Provincia", "Telefono", "Correo de empresa", "Sueldo"});
 	            }else{
